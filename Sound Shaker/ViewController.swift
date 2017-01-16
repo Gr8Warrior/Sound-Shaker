5c67070ad94c0c15d4b2157ef380b91f1c75b446
shailendrasuriyal <shailendra.suriyal@intimetec.com>
2017-01-16 13:00:09 +0530

    shaking the device makes sound

--- Sound Shaker/ViewController.swift
+++ Sound Shaker/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
-
+import  AVFoundation
 class ViewController: UIViewController {
-
+    
+    var player = AVAudioPlayer()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        print("Device Shaked ")
+        var sounds = ["boing","explosion","hit","knife","shoot","swish","wah","warble"]
+        let random = Int(arc4random_uniform( UInt32(UInt(sounds.count)) ))
+        
+        if event?.subtype == UIEventSubtype.motionShake {
+          let fileName = sounds[random]
+            print(fileName)
+            let filePath = Bundle.main.path(forResource: "sounds/"+fileName, ofType: "mp3")
+            do {
+                try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: filePath!))
+                player.play()
+                
+            }catch{
+                //Process Error
+            }
+            
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
